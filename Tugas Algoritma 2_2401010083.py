2f15e83ea7019771433e2808d91a1d99eb48b4bb
Chelsea Dewi Pertami <chelseadewipertami@gmail.com>
2024-11-28 22:07:12 +0800

    Tugas Algoritma Pertemuan 2_2401010083

---
+++ Tugas Algoritma 2_2401010083.py
@@ -0,0 +1,21 @@
+"""
+Nama: Ni Putu Chelsea Dewi Pertami
+NIM: 2401010083
+Kelas: K
+Prodi: IF- KAB
+Dosen: I Made Gede Sri Artha
+Tanggal: 28 November 2024
+TUGAS: konversi satuan dari meter ke cm dan implementasi ke kode python dengan menerapkan konsep komentar, input dan output
+"""
+
+# Program konversi satuan dari meter ke centimeter
+
+# Masukkan angka yang akann diubah (angka dalam meter)
+meter = float(input("Masukkan angka dalam meter: "))
+
+# Melakukan konversi ke centimeter
+centimeter = meter * 100  # 1 meter sama dengan 100 cm
+
+# Menampilkan hasil konversi ke layar
+print(f"Hasil dalam centimeter: {int(centimeter)} cm")
+
